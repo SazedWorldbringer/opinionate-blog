96309e010d0b1f9340b52a122af1ebcc9d64a07a
Atharva Pardeshi <atharva.exe@gmail.com>
2024-02-18 20:38:42 +0530

    refactor: comments

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,26 +7,26 @@ export function Footer() {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
+        <div className="py-28 flex flex-col lg:flex-row items-center justify-between">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             Empower through self-expression.
           </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <Button size='lg'>
-              <Link
-                href="mailto:eoc1810@gmail.com"
-                // className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-              >
-                Get in touch
-              </Link>
-            </Button>
-            {/* <a
+          {/*<div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">*/}
+          <Button size='lg'>
+            <Link
+              href="mailto:eoc1810@gmail.com"
+            // className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+            >
+              Get in touch
+            </Link>
+          </Button>
+          {/* <a
               href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
               className="mx-3 font-bold hover:underline"
             >
               View on GitHub
-            </a> */}
-          </div>
+            </a> 
+          </div>*/}
         </div>
       </Container>
     </footer>
